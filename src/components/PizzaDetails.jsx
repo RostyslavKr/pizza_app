9491c4cd1c57c756b8b29a236993e700aca3bd7b
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2025-02-13 06:14:32 +0200

    update pizzadetails page

--- src/components/PizzaDetails.jsx
+++ src/components/PizzaDetails.jsx
@@ -25,13 +25,15 @@ export const PizzaDetails = () => {
 
   return (
     <div class="bg-white">
-      <div class="flex">
-        <img src={pizza.image} alt="pizza" width={400} height={400} />
+      <div class="flex justify-around gap-4 m-auto w-[80%] pt-38">
+        <img src={pizza.image} alt="pizza" width={450} height={450} />
         <div>
-          <p>{pizza.title}</p>
+          <p class="text-4xl font-semibold pb-[10px]">{pizza.title}</p>
           <p>{pizza.weight}g</p>
-          <p>{pizza.description}</p>
-          <form class="inline-flex items-center justify-around w-full bg-gray-100  rounded-full">
+          <p class="text-lg font-medium tracking-wide pb-[10px]">
+            {pizza.description}
+          </p>
+          <form class="inline-flex items-center justify-around w-[80%] bg-gray-100  rounded-full">
             <label class="block w-full cursor-pointer text-center">
               <input
                 type="radio"
@@ -51,31 +53,66 @@ export const PizzaDetails = () => {
               </span>
             </label>
           </form>
-          <div class="flex">
-            <button>Add ingridients</button>
+          <div class="flex gap-16 pb-[20px] pt-[20px]">
+            <button class="rounded-xl shadow-md bg-[#C74C33] text-white hover:scale-105 duration-200 ease-in p-2 text-xl font-semibold tracking-wide">
+              Add ingridients
+            </button>
             <div class="flex items-center">
-              <button onClick={handleClickInc}>+</button>
-              <p>{counter}</p>
-              <button onClick={handleClickDecr}>-</button>
+              <button class="text-2xl font-bold" onClick={handleClickInc}>
+                <svg
+                  width={25}
+                  height={25}
+                  class="stroke-black hover:stroke-orange-700 duration-250 ease-in"
+                >
+                  <use href="./images/sprite.svg#plus"></use>
+                </svg>
+              </button>
+              <p class=" text-xl rounded-sm border border-black p-2 mr-2 ml-2">
+                {counter}
+              </p>
+              <button class="text-2xl font-bold" onClick={handleClickDecr}>
+                <svg
+                  width={25}
+                  height={25}
+                  class="stroke-black hover:stroke-orange-700 duration-250 ease-in"
+                >
+                  {" "}
+                  <use href="./images/sprite.svg#minus"></use>
+                </svg>
+              </button>
             </div>
           </div>
 
-          <div class="flex">
-            <p>Ingridients: {priceIngredient}$</p>
-            <p>Total: {price}$</p>
+          <div class="flex gap-16 pb-[50px]">
+            <p class="text-2xl font-medium">
+              Ingridients:{" "}
+              <span class="text-3xl font-bold">{priceIngredient}$</span>
+            </p>
+            <p class="text-2xl font-medium">
+              Total: <span class="text-3xl font-bold">{price}$</span>
+            </p>
           </div>
-          <button>Add to cart</button>
+          <button class="rounded-xl shadow-md bg-[#C74C33] text-white hover:scale-105 duration-200 ease-in p-2 text-xl font-semibold tracking-wide">
+            Add to cart
+          </button>
         </div>
       </div>
-      <div>
-        <p>Ingridients for pizza</p>
-        <ul>
+      <div class="pb-20">
+        <p class="text-center text-4xl font-bold pt-28 pb-16">
+          Ingridients for pizza
+        </p>
+        <ul class="grid grid-row-3 grid-cols-6 gap-x-8 gap-y-20 px-[30px]">
           {ingredients.map((i, index) => (
-            <li key={index}>
-              <img src={i.image} alt="ing" width={50} height={50} />
-              <p>{i.name}</p>
-              <p>{i.weight}</p>
-              <p>{i.price}</p>
+            <li key={index} class="px-[10px]">
+              <div class="flex justify-center items-center">
+                <img src={i.image} alt="ing" width={100} height={100} />
+              </div>
+
+              <p class="text-center text-base font-bold">{i.name}</p>
+              <div class="flex justify-between items-center">
+                <p>{i.weight}g</p>
+                <p>{i.price}$</p>
+              </div>
             </li>
           ))}
         </ul>
--- src/pages/PizzaDetailsPage.jsx
+++ src/pages/PizzaDetailsPage.jsx
@@ -1,9 +1,13 @@
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 import { PizzaDetails } from "../components/PizzaDetails";
 
 export const PizzaDetailsPage = () => {
   return (
     <>
+      <Header />
       <PizzaDetails />
+      <Footer />
     </>
   );
 };
